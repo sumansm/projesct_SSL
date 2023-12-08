583dd175cc4941d03bdf14fcf1799c42a7a95680
teamdev <teamdev@convesio.com>
2023-12-08 09:26:46 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello from Dockerized Node.js App!\n');
 });
 
-// const PORT = process.env.PORT || 3000;
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
