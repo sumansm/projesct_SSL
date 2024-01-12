31959ebaee8e3e087ef33ccf15232ebeae150ed1
teamdev <teamdev@convesio.com>
2024-01-12 07:20:43 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Dockerized Node.js App!\n');
+  res.end('Hello from Dockerized Node.js App! Updated code \n');
 });
 
 const PORT = process.env.PORT || 3000;
