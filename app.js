f090000c0694ead97948b284ca5528a5ff626818
Suman Mondal <109333478+sumansm@users.noreply.github.com>
2024-01-31 13:35:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
-const http = require('http');
+const express = require('express');
+const https = require('https');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Dockerized Node.js App! Updated code \n');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });
 });
+app.post('/check', (req, res) => {
+    const { domain } = req.body;
+  
+    const options = {
+      method: 'GET',
+      hostname: domain,
+      port: 443,
+    };
+  
+    const request = https.request(options, (response) => {
+      const certificate = response.socket.getPeerCertificate();
+  
+      if (certificate) {
+        res.json({
+          success: true,
+          certificateData: {
+            subject: certificate.subject,
+            issuer: certificate.issuer,
+            valid_from: certificate.valid_from,
+            valid_to: certificate.valid_to,
+          },
+        });
+      } else {
+        res.json({
+          success: false,
+          error: 'No SSL certificate found for the specified domain.',
+        });
+      }
+    });
+  
+    request.on('error', (error) => {
+      res.json({
+        success: false,
+        error: error.message,
+      });
+    });
+  
+    request.end();
+  });
+  
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
